80c7a7c6d18c16c70f1d994b6dc9fa73282eb265
Nakatake <tonarinototorototoro8972@gmail.com>
2022-02-28 20:19:58 +0900

    02/28 @nakashima-takeo

--- hellotaleo.c
+++ hellotaleo.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-int main()
+
+struct pair
 {
-   printf("hello world\n");
-   printf("warudokunn\n");
-   printf("ついでに追加したよ\n");
-   printf("ついでに追加したよ\n");
+    int fst;
+    int snd;
+};
+
+void foo (int **pp1, int **pp2)
+{
+    int x = **pp1 - **pp2;
+    **pp1 -= x;
+    **pp2 += x;
+    return;
+}
+
+int main(void)
+{
+    struct pair p1 = {1, 2};
+    struct pair p2 = {3, 4};
+    struct pair *pp1 = &p1;
+    struct pair *pp2 = &p2;
+    int *p3 = &pp1 -> snd;
+    int *p4 = &pp2 -> fst;
+    foo(&p4, &p3);
+    printf("p1 = (%d, %d), p2 = (%d, %d)",
+             p1.fst, p1.snd, p2.fst, p2.snd);
+    return 0;
 }
